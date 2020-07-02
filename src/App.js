aa3b2ed2449d6b767088d7bf54914823d06bec25
beauwilliams <beau.williams@icloud.com>
2020-07-02 12:18:37 +1000

    Refactored testimonials to 'recommendations'

--- src/App.js
+++ src/App.js
@@ -1,61 +1,59 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import React, { Component } from 'react'
+import ReactGA from 'react-ga'
+import $ from 'jquery'
+import './App.css'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import About from './Components/About'
+import Resume from './Components/Resume'
+import Contact from './Components/Contact'
+import Recommendations from './Components/Recommendations'
+import Portfolio from './Components/Portfolio'
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      foo: 'bar',
+      function: 'constructor', //BW > not sure what this does
       resumeData: {}
-    };
+    }
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
-
+    ReactGA.initialize('UA-110570651-1')
+    ReactGA.pageview(window.location.pathname)
   }
 
-  getResumeData(){
+  getResumeData () {
     $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
+      url: '/resumeData.json',
+      dataType: 'json',
       cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
+      success: function (data) {
+        this.setState({ resumeData: data })
       }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
+      error: function (xhr, status, err) {
+        console.log(err)
+        alert(err)
       }
-    });
+    })
   }
 
-  componentDidMount(){
-    this.getResumeData();
+  componentDidMount () {
+    this.getResumeData()
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+      <div className='App'>
+        <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Recommendations data={this.state.resumeData.recommendations} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
